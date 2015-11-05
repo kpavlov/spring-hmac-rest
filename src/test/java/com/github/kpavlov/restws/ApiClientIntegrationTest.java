9e5fa0cf495274c7cbd05d778366178731e368e1
kpavlov <git@konstantinpavlov.net>
2015-11-05 07:24:59 +0200

    ApiClientIntegrationTest improved

--- src/test/java/com/github/kpavlov/restws/ApiClientIntegrationTest.java
+++ src/test/java/com/github/kpavlov/restws/ApiClientIntegrationTest.java
@@ -10,12 +10,14 @@ import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.IntegrationTest;
 import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.http.HttpStatus;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.web.client.HttpClientErrorException;
 
 import static org.apache.commons.lang3.RandomStringUtils.randomAlphanumeric;
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNotNull;
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.*;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @SpringApplicationConfiguration(classes = SpringRestHmacApplication.class)
@@ -31,15 +33,39 @@ public class ApiClientIntegrationTest {
     @Before
     public void beforeTest() {
         client = new ApiClient("localhost", port);
-        client.setCredentials("user", "secret");
         client.init();
     }
 
     @Test
     public void testEcho() {
+        client.setCredentials("user", "secret");
         Foo request = new Foo(randomAlphanumeric(5));
         final FooResponseWrapper response = client.echo(request, FooResponseWrapper.class);
         assertNotNull(response);
         assertEquals(request, response.getData());
+    }
+
+    @Test
+    public void testFailWhenPasswordIsBad() {
+        client.setCredentials("user", "bad password");
+        Foo request = new Foo(randomAlphanumeric(5));
+        try {
+            client.echo(request, FooResponseWrapper.class);
+            fail("Http Error 403 is expected");
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.FORBIDDEN));
+        }
+    }
+
+    @Test
+    public void testFailWhenUserIsUnknown() {
+        client.setCredentials("unknownUser", "any password");
+        Foo request = new Foo(randomAlphanumeric(5));
+        try {
+            client.echo(request, FooResponseWrapper.class);
+            fail("Http Error 403 is expected");
+        } catch (HttpClientErrorException e) {
+            assertThat(e.getStatusCode(), is(HttpStatus.FORBIDDEN));
+        }
     }
 }
